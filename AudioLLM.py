86a0350ebbb369e17b9500b68ed32a6e9db73d60
Josep Crego <jmcrego@gmail.com>
2026-02-05 18:59:25 +0100

    debug

--- AudioLLM.py
+++ AudioLLM.py
@@ -79,14 +79,14 @@ class AudioLLM(torch.nn.Module):
         pad_mask = ~txt_mask
 
         # Projector forward
-        proj_embs, _ = self.projector(input_embeds)      # [B, S_max, D_llm]
+        proj_embs, _ = self.projector(input_embeds) # [B, S_max, D_llm]
         B, S_max, D = proj_embs.shape
         assert S_max == target_ids.size(1), f"Expected S_max={target_ids.size(1)}, got {S_max}"
         assert D == self.llm_embedding_dim, f"Expected D={self.llm_embedding_dim}, got {D}"
         audio_norm = proj_embs.norm(dim=-1).mean()
 
         # LLM Embedder forward
-        text_embs = self.llm_embedder(target_ids) # [B, S_max, D_llm]
+        text_embs = self.llm_embedder(target_ids) # [B, T_max, D_llm]
         B, T_max, D = text_embs.shape
         assert T_max == proj_embs.size(1), f"Expected T_max={proj_embs.size(1)}, got {T_max}"
         assert D == self.llm_embedding_dim, f"Expected D={self.llm_embedding_dim}, got {D}"
