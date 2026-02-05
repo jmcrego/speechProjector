1f920a269ae15659b8de41d694db18af2daef57c
Josep Crego <jmcrego@gmail.com>
2026-02-05 18:50:45 +0100

    debug

--- Dataset.py
+++ Dataset.py
@@ -12,18 +12,9 @@ from torch.utils.data import Dataset, BatchSampler
 logger = logging.getLogger("Dataset")
 
 def collate_fn(batch):
-    # def ensure_tensor(x):
-    #     if isinstance(x, torch.Tensor):
-    #         return x
-    #     return torch.tensor(x, dtype=torch.long)
-
-    assert "target_ids" in batch[0]
     target_ids = torch.stack([x["target_ids"] for x in batch], dim=0)  # (B, T)
-
-    assert "pt_path" in batch[0] and "offset" in batch[0]
     pt_paths = [x["pt_path"] for x in batch] # List[str] (B,)
     offsets = [x["offset"] for x in batch] # List[int] (B,)
-    # offsets = ensure_tensor(offsets) # (B,)
 
     return {
         "pt_paths": pt_paths,         # List[str] (B,)
