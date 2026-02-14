3f2a386f0bf93f8a086224595b195b312d573726
Josep Crego <jmcrego@gmail.com>
2026-02-14 19:01:38 +0100

    debug

--- scripts/LibriSpeech.py
+++ scripts/LibriSpeech.py
@@ -71,7 +71,7 @@ def main():
             h = t_out // 3600
             m = (t_out % 3600) // 60
             s = t_out % 60
-            print(f"Created {n_out} out of {n_in} audio segments ({100*n_out/n_in:.2f}%). Duration is {h}:{m}:{s:05.2f} ({t_out/n_out:.1f} secs/file)\n")
+            print(f"Created {n_out} out of {n_in} audio segments ({100*n_out/n_in:.2f}%). Duration is {h}:{m}:{s:05.2f} ({t_out/n_out:.1f} secs/file)")
 
 
 if __name__ == "__main__":
