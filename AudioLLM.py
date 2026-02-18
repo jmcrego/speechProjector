bcef9dd850f033b3a3e51cef7abff9e729d4531f
Josep Crego <jmcrego@gmail.com>
2026-02-18 09:26:16 +0100

    debug

--- AudioLLM.py
+++ AudioLLM.py
@@ -159,7 +159,7 @@ class AudioLLM(torch.nn.Module):
         text_embs_norm = F.normalize(text_embs, dim=-1)
         logger.info(f"proj_embs_norm: {proj_embs_norm.shape}, dtype={proj_embs_norm.dtype}")
         logger.info(f"text_embs_norm: {text_embs_norm.shape}, dtype={text_embs_norm.dtype}")
-        logits = (proj_embs_norm @ text_embs_norm.t()) / self.temp_contrast # Similarity matrix [B, B], scale by temperature
+        logits = (proj_embs_norm @ text_embs_norm.T) / self.temp_contrast # Similarity matrix [B, B], scale by temperature
         targets = torch.arange(logits.size(0), device=logits.device) # Targets are diagonal
         loss_contrast = F.cross_entropy(logits, targets)
         dout['loss_contrast'] = loss_contrast.item()
