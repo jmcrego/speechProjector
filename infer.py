44675fb753fe327b855cecd5c0d3f6425c36e34a
Josep Crego <jmcrego@gmail.com>
2026-02-20 16:51:50 +0100

    debug

--- infer.py
+++ infer.py
@@ -59,7 +59,7 @@ if __name__ == "__main__":
     device, dtype = get_device_dtype()
     logger.info(f"device: {device}, dtype: {dtype}")
 
-    prompt = (f"Input:\n<extra_id_0>\nRepeat the above {args.lang} Input text:\n")
+    prompt = (f"Input:\n<extra_id_0>\nRepeat the above {args.lang} Input text using the same language:\n")
 #    prompt = (f"Input:\n<extra_id_0>\nTranslate the above {args.lang} Input text into French:\n")
 #    prompt = (f"Input:\n<extra_id_0>\nQuestion: Is the above Input text written in German? (write only Yes or No)\nAnswer:")
 
